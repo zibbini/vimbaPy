a2d690bd65b8dce39a0caff470548f6f541a51b9
zibbini <ziadibbini@gmail.com>
2020-03-20 15:51:49 +0000

    Add installation script

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+# from vimbaPy import __verson__
+
+setup(name='vimbaPy',
+      version=0.1,
+      description="vimbaPy is a package containing helper/convenience functions for pymba and VimbaPython.",
+      keywords='python, python3, opencv, machine vision, computer vision, vimba, allied vision',
+      author='zibbini',
+      author_email='ziadibbini@gmail.com',
+      license='MIT',
+      packages=[
+          'vimbaPy',
+      ]
+)
+
