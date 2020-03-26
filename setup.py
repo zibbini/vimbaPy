3f3997a4cd63db256f53e390e7acf8cc86f19651
zibbini <ziadibbini@gmail.com>
2020-03-26 22:40:56 +0000

    Update version info

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
-# from vimbaPy import __verson__
+from vimbaPy import __version__
+
 
 setup(name='vimbaPy',
-      version=0.1,
+      version=__version__,
       description="vimbaPy is a package containing helper/convenience functions for pymba and VimbaPython.",
       keywords='python, python3, opencv, machine vision, computer vision, vimba, allied vision',
       author='zibbini',
